500f93389c994c842ef964ebf2c4641fb514cd08
Ondrej Platek <ondrej.platek@seznam.cz>
2025-02-21 11:28:27 +0100

    updates ollama_serve and gpu-{python,bash}

--- bin/ollama_serve.sh
+++ bin/ollama_serve.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ollama_host="0.0.0.0:11434"
+mem=64
+gpu_mem=48
 
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
@@ -9,4 +11,8 @@ export PATH="$SCRIPT_DIR:$PATH"
 
 port="$(echo $ollama_host | cut -d: -f2)"
 
-OLLAMA_HOST="$ollama_host" OLLAMA_KEEP_ALIVE=-1 gpu-bash --jobname "O${port}" --async false ollama serve
+OLLAMA_MODELS="/lnet/work/projects/LLM/ollama" \
+OLLAMA_HOST="$ollama_host" \
+OLLAMA_KEEP_ALIVE=-1 \
+gpu-bash --mem "$mem" --gpu_mem "$gpu_mem" --jobname "O${port}" --async false \
+ollama serve
